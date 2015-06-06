713ee4fea1a3c5318091b4709e86f33fb8e924e5
Ross Dakin <rossdakin@gmail.com>
2015-06-06 15:41:29 -0700

    Tweak active_admin and devise settings

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,15 +1,16 @@
+# coding: utf-8
 ActiveAdmin.setup do |config|
   # == Site Title
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Made With Open Data Api"
+  config.site_title = "Made With Open Data — Admin"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/admin/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)

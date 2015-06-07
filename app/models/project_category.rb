44cf2b870795c66a4059e5eafb52b295f704dcb4
Ross Dakin <rossdakin@gmail.com>
2015-06-07 14:03:55 -0700

    Add validations to categories and types

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,4 +1,7 @@
 class ProjectCategory < ActiveRecord::Base
   belongs_to :project
   belongs_to :category
+
+  validates :project, presence: true
+  validates :category, presence: true, uniqueness: { scope: :project }
 end
--- app/models/project_type.rb
+++ app/models/project_type.rb
@@ -1,4 +1,7 @@
 class ProjectType < ActiveRecord::Base
   belongs_to :project
   belongs_to :type
+
+  validates :project, presence: true
+  validates :type, presence: true, uniqueness: { scope: :project }
 end
